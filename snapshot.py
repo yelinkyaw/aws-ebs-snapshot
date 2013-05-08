7c36c3f039f0a8fd9bfd5356247086f1a1ed60a2
Ye Lin Kyaw <yelin@amdon.com>
2013-05-08 16:09:06 +0800

    Add timestamp to STD Out

--- snapshot.py
+++ snapshot.py
@@ -22,9 +22,9 @@ max_snapshot = 5
 con = boto.ec2.connect_to_region(aws_region, aws_access_key_id=aws_access_key, aws_secret_access_key=aws_secret_key)
 
 # Create Snapshot
-print 'Creating snapshot of volume:', aws_volume_id
+print datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), 'Creating snapshot of volume:', aws_volume_id
 new_snapshot = con.create_snapshot(aws_volume_id, 'Automated Snapshot')
-print 'Started creating snapshot:', new_snapshot.id
+print datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), 'Started creating snapshot:', new_snapshot.id
 
 # Get snapshot list
 snapshot_raw = con.get_all_snapshots(filters={'volume-id': aws_volume_id})
@@ -40,6 +40,6 @@ snapshots = collections.OrderedDict(sorted(snapshot_list.items()))
 # Delete the oldest snapshot
 if len(snapshots.keys())>max_snapshot:
 	oldest_snapshot_id = snapshots[snapshots.keys()[0]]
-	print 'Deleting snapshot:', oldest_snapshot_id
+	print datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), 'Deleting snapshot:', oldest_snapshot_id
 	con.delete_snapshot(oldest_snapshot_id)
-	print 'Finish deleting snapshot'
+	print datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), 'Finish deleting snapshot'
